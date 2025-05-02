ef3d3a263d25880d2238e745ec6c59cf7ae9cf50
andreasLoetzsch <andreas.lotzsch@gmail.com>
2025-05-02 16:23:05 +0200

    feat: all tests working

--- src/test/calculator.test.jsx
+++ src/test/calculator.test.jsx
@@ -26,4 +26,61 @@ describe("Calculator test", async () => {
 
     fireEvent.click(buttonAC);
   });
+  it("testing subtraction", async () => {
+    render(<Calcultaor />);
+
+    const buttonNine = screen.getByRole("button", { name: "9" });
+    const buttonThree = screen.getByRole("button", { name: "3" });
+    const buttonMinus = screen.getByRole("button", { name: "-" });
+    const buttonEquals = screen.getByRole("button", { name: "=" });
+    const buttonAC = screen.getByRole("button", { name: "AC" });
+
+    fireEvent.click(buttonNine);
+    fireEvent.click(buttonMinus);
+    fireEvent.click(buttonThree);
+    fireEvent.click(buttonEquals);
+
+    await waitFor(() => {
+      expect(screen.getByText("6", { selector: "p" })).toBeInTheDocument();
+    });
+    fireEvent.click(buttonAC);
+  });
+  it("testing multiplication", async () => {
+    render(<Calcultaor />);
+
+    const buttonFive = screen.getByRole("button", { name: "5" });
+    const buttonThree = screen.getByRole("button", { name: "3" });
+    const buttonMultiplication = screen.getByRole("button", { name: "*" });
+    const buttonEquals = screen.getByRole("button", { name: "=" });
+    const buttonAC = screen.getByRole("button", { name: "AC" });
+
+    fireEvent.click(buttonFive);
+    fireEvent.click(buttonMultiplication);
+    fireEvent.click(buttonThree);
+    fireEvent.click(buttonEquals);
+
+    await waitFor(() => {
+      expect(screen.getByText("15", { selector: "p" })).toBeInTheDocument();
+    });
+    fireEvent.click(buttonAC);
+  });
+  it("testing division", async () => {
+    render(<Calcultaor />);
+
+    const buttonSix = screen.getByRole("button", { name: "6" });
+    const buttonThree = screen.getByRole("button", { name: "3" });
+    const buttonDivision = screen.getByRole("button", { name: "/" });
+    const buttonEquals = screen.getByRole("button", { name: "=" });
+    const buttonAC = screen.getByRole("button", { name: "AC" });
+
+    fireEvent.click(buttonSix);
+    fireEvent.click(buttonDivision);
+    fireEvent.click(buttonThree);
+    fireEvent.click(buttonEquals);
+
+    await waitFor(() => {
+      expect(screen.getByText("2", { selector: "p" })).toBeInTheDocument();
+    });
+    fireEvent.click(buttonAC);
+  });
 });
